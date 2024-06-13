e8b8a7e83b0e178671a1a116043b4f909d74469f
dchen <dchen@dchens-MacBook-Air.local>
2024-06-13 20:22:25 +0800

    update

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+docker build --build-arg UBUNTU_VERSION=22.04 -t amdsev-build:22.04 docker/
+
+run2204() {
+  docker run --init --rm -v $PWD/.ccache:/root/.ccache -v $PWD:$PWD -w $PWD amdsev-build:22.04 "$@"
+}
+
+run2204 ./build.sh --package qemu kernel host
