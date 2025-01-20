77747516b695e1cd70ea8ae6f03ac321131929e4
dong <sakura.chdong@gmail.com>
2025-01-20 19:37:22 +0800

    fix the docker building

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -ex
 docker build --build-arg UBUNTU_VERSION=22.04 -t amdsev-build:22.04 docker/
 
 run2204() {
-  docker run --init --rm -v $PWD/.ccache:/root/.ccache -v $PWD:$PWD -w $PWD amdsev-build:22.04 "$@"
+  docker run --init --rm -v /boot:/boot -v $PWD/.ccache:/root/.ccache -v $PWD:$PWD -w $PWD amdsev-build:22.04 "$@"
 }
 
-run2204 ./build.sh --package qemu kernel host
+run2204 ./build.sh --package
